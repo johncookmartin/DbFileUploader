3148f2b76acbad9f78f81eb2b4c8b4094bd942b3
John Cook-Martin <johncookmartin@gmail.com>
2025-05-29 20:54:39 -0400

    Added general purpose access so I can configure this for multiple file types.

---
+++ DbFileUploader/ConsoleUI/InputHandler.cs
@@ -0,0 +1,25 @@
+﻿namespace DbFileUploader.ConsoleUI;
+public static class InputHandler
+{
+    public static string GetConfigFile(string[] args)
+    {
+        string? filePath = null;
+
+        if (args.Length == 2)
+        {
+            filePath = args[1];
+        }
+
+        while (string.IsNullOrWhiteSpace(filePath))
+        {
+            Console.WriteLine("Enter Config File Path: ");
+            filePath = Console.ReadLine();
+            if (string.IsNullOrWhiteSpace(filePath))
+            {
+                Console.WriteLine("Please enter a file path.");
+            }
+        }
+
+        return filePath;
+    }
+}
---
+++ DbFileUploader/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace DbFileUploader;
+
+internal class Program
+{
+    public static object CSVDataUploader { get; private set; }
+
+    static void Main(string[] args)
+    {
+        if (args.Length == 0)
+        {
+            Console.WriteLine("No arguments provided. Please provide a file to upload.");
+            return;
+        }
+        string fileName = args[0];
+        string fileType = Path.GetExtension(fileName).ToLowerInvariant();
+
+        switch (fileType)
+        {
+            case ".csv":
+                CSVDataUploader.Program.Main(args);
+                break;
+            case ".json":
+                JsonDataUploader.Program.Main(args);
+                break;
+            default:
+                Console.WriteLine($"Unsupported file type: {fileType}. Supported types are .csv, .json, and .xml.");
+                break;
+        }
+
+    }
+}
